86fc07243fd5632e73c002b5a3829af2d7d38789
imkozin <ivan.kozin@hotmail.com>
2023-08-20 01:07:38 +0300

    fixed navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
